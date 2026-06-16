5e5dde3a681d4310f95001b2b8f89228c33e2364
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-06-16 23:03:38 +0530

    feat: update README and remove draft binary search implementations; add FindFirstOccurence implementation

---
+++ binarysearch/FindFirstOccurence.java
@@ -0,0 +1,29 @@
+package binarysearch;
+
+public class FindFirstOccurence {
+    static int FindFirstOccurenceOfElement(int arr[], int t) {
+        int s = 0, e = arr.length - 1, l = -1;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            if (arr[m] == t) {
+                l = m;
+                e = m - 1;
+            }
+
+            if (arr[m] < t) {
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        return (l == -1) ? -1 : l;
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 2, 2, 8, 8, 10 }, t = 8;
+        System.out.println(FindFirstOccurenceOfElement(arr, t));
+    }
+
+}
+// }
