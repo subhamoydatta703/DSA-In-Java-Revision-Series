dca7aa689d769c27943c858b1557543825b57315
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-07-10 13:06:01 +0530

    feat: enhance Basic Concepts of Linked List; add traversal and insertion methods, update README

--- linked_list/Basic_Concepts.java
+++ linked_list/Basic_Concepts.java
@@ -1,28 +1,61 @@
 package linked_list;
 
-class  Node{
+class Node {
     int data;
     Node next;
+
     // method 1: flexible
-    Node(int data){
+    Node(int data) {
         this.data = data;
         this.next = null;
     }
 
-
     // method 2: simpler constructor design: have a drawback: not flexible
-    //  new Node(value, next node)
+    // new Node(value, next node)
     // Node(int data, Node next){
-    //     this.data = data;
-    //     this.next = next;
+    // this.data = data;
+    // this.next = next;
     // }
 }
+
 public class Basic_Concepts {
+
+    // node traversal
+    static void nodeTraversal(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.println("Data: " + curr.data);
+            curr = curr.next;
+        }
+        System.out.println("Null");
+    }
+
+    static Node instertAtHead(Node head, int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+
+        return newNode;
+    }
+
+    static Node insertAtTail(Node head, int val) {
+        Node newNode = new Node(val);
+        if (head == null)
+            return newNode;
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+        return head;
+    }
+
     public static void main(String[] args) {
 
         // create a linked list
 
-        // drawback of this simple structure is that i have to write down nodes backwords. Which is not flexsible, but method 1 of the cromnstructor is more flexible and shpould use that instead of the method 2
+        // drawback of this simple structure is that i have to write down nodes
+        // backwords. Which is not flexsible, but method 1 of the cromnstructor is more
+        // flexible and shpould use that instead of the method 2
 
         // method 2:
         // Node second = new Node(20, null);
@@ -31,15 +64,27 @@ public class Basic_Concepts {
         // System.out.println(head.data);
 
         // Node head = new Node(10, new Node(20, new Node(30, null)));
-        
+
         Node head = new Node(10);
         Node second = new Node(20);
         Node third = new Node(30);
         head.next = second;
         second.next = third;
 
+        // System.out.println("Head: "+head);
+        // System.out.println("Head data: "+head.data);
+        // System.out.println("Head next: "+head.next);
+        // System.out.println("second: "+second);
+        // System.out.println("second data: "+second.data);
+        // System.out.println("second next: "+second.next);
+        // System.out.println("third: "+third);
+        // System.out.println("third data: "+third.data);
+        // System.out.println("third next:"+third.next);
+
+        insertAtTail(head, 40);
+        nodeTraversal(head);
+
 
     }
-    
-}
 
+}
